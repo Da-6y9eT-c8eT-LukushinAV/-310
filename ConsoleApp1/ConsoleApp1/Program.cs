c7fd12a359bda502741021c9266715b74869aa3c
Da-6y9eT-c8eT-LukushinAV <lukushinav@mer.ci.nsu.ru>
2025-03-03 09:59:54 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 5, 3, 8, 6, 2, 7, 4, 1 };
+
+            Console.WriteLine("Исходный массив:");
+            Console.WriteLine(SortingAlgorithms.PrintArray(array));
+
+            int[] combSorted = SortingAlgorithms.CombSort((int[])array.Clone());
+            Console.WriteLine("Отсортировано методом расческой:");
+            Console.WriteLine(SortingAlgorithms.PrintArray(combSorted));
+
+            int[] shellSorted = SortingAlgorithms.ShellSort((int[])array.Clone());
+            Console.WriteLine("Отсортировано методом Шелла:");
+            Console.WriteLine(SortingAlgorithms.PrintArray(shellSorted));
+
+            int[] bubbleSorted = SortingAlgorithms.BubbleSort((int[])array.Clone());
+            Console.WriteLine("Отсортировано пузырьковой сортировкой:");
+            Console.WriteLine(SortingAlgorithms.PrintArray(bubbleSorted));
+
+            int target = 6;
+            int? found = SortingAlgorithms.SelectId(array, target);
+            Console.WriteLine(found.HasValue
+                ? $"Элемент {target} найден."
+                : $"Элемент {target} не найден.");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/SortingAlgorithms.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class SortingAlgorithms
+    {
+        private int[] _array;
+        public int Index { get; private set; }
+
+        private SortingAlgorithms() { }
+
+        private SortingAlgorithms(int[] array, int index)
+        {
+            _array = array;
+            Index = index;
+        }
+
+        private static void Swap(ref int value1, ref int value2)
+        {
+            (value1, value2) = (value2, value1);
+        }
+
+        private static int GetNextStep(int step)
+        {
+            step = step * 1000 / 1247;
+            return step > 1 ? step : 1;
+        }
+
+        public static int[] CombSort(int[] array)
+        {
+            int length = array.Length;
+            int step = length - 1;
+
+            while (step > 1)
+            {
+                for (int i = 0; i + step < length; i++)
+                {
+                    if (array[i] > array[i + step])
+                    {
+                        Swap(ref array[i], ref array[i + step]);
+                    }
+                }
+                step = GetNextStep(step);
+            }
+
+            for (int i = 1; i < length; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < length - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+            return array;
+        }
+
+        public static int? SelectId(int[] array, int target)
+        {
+            return array.Contains(target) ? target : (int?)null;
+        }
+
+        public static int[] ShellSort(int[] array)
+        {
+            int size = array.Length;
+            for (int gap = size / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < size; i++)
+                {
+                    int temp = array[i];
+                    int j;
+                    for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+                    {
+                        array[j] = array[j - gap];
+                    }
+                    array[j] = temp;
+                }
+            }
+            return array;
+        }
+
+        public static int[] BubbleSort(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+            return array;
+        }
+
+        public static string PrintArray(int[] array)
+        {
+            return string.Join(" ", array);
+        }
+    }
+}
